ce8324f05190ffada87c50b93387a8eab408250d
Joel Guizar <joelguizar@gmail.com>
2017-11-03 13:15:16 -0700

    cityData

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+//we need mapstatetoProps to
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData){
+    return (
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th> City </th>
+            <th> Temperature </th>
+            <th> Pressure </th>
+            <th> Humidity </th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
+
+//pulls weather from state, this is saying 'state.weather' essentially
+function mapStateToProps({weather}){
+  //state.weather comes from the defined weather in the reducers
+  return {weather} // identical to {weather: state.weather}
+}
+
+//we're exporting the connected version of WeatherList
+export default connect(mapStateToProps)(WeatherList);
