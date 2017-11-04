ed03a2b93019e4798a9a8a9863cc7878c86f2a69
Joel Guizar <joelguizar@gmail.com>
2017-11-04 01:19:12 -0700

    final note

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -4,6 +4,7 @@ class GoogleMap extends Component {
   componentDidMount(){
     //third party library, that needs an html element where to render the map
     //the refs tell it which html element
+    //different from a library such as react-google-maps
     new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
